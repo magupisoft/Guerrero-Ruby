ca5126f1ec5e5745683051f2a96334a9b58c516e
Manuel Gutiérrez <lp.gutierrezpineda@gmail.com>
2015-05-13 17:20:42 -0500

    Level 6 OK

--- tariacuri-intermediate/player.rb
+++ tariacuri-intermediate/player.rb
@@ -23,26 +23,29 @@ class Player
 		puts "Empties around #{@empties_around.inspect}"
 		@captives_around = @spaces.select{|u| u.captive? }
 		puts "Captives around #{@captives_around.inspect}"
-		
+		@ticking_around = @spaces.select{|u| u.ticking? }
+		puts "Ticking around #{@ticking_around.inspect}"
 		
 		@enemies_near = @directions.select{|d| @warrior.feel(d).enemy? }
 		puts "Enemies near #{@enemies_near.inspect}"
 		@empties_near = @directions.select{|d| @warrior.feel(d).empty? and not @warrior.feel(d).stairs?}
 		puts "Empties near #{@empties_near.inspect}"
 		@captives_near = @directions.select{|d| @warrior.feel(d).captive? }
-		puts "Captives near #{@captives_near.inspect}"
-		
+		puts "Captives near #{@captives_near.inspect}"		
 		@stairs_near = @directions.select{|d| @warrior.feel(d).stairs? }
 		puts "Stairs near #{@stairs_near.inspect}"
+		@ticking_near = @directions.select{|d| @warrior.feel(d).ticking? }
+		puts "Ticking near #{@ticking_near.inspect}"
 	end
 	
 	def take_action
 		return move_to victory if @spaces.empty?
 		return rest if should_rest?
 		return take_shelter if must_rest?
+		return deactive_bomb if @ticking_near.any? or @ticking_around.any?
 		return bind_enemy if @enemies_near.any? and @enemies_near.length > 1
-		return attack_enemy if @enemies_near.any?
 		return rescue_captive if @captives_near.any?
+		return attack_enemy if @enemies_near.any? and not @ticking_around.any?
 		return move_to next_empty unless (@captives_around.any? or @enemies_around.any?) and @stairs_near.empty?
 		return move_to near_captive_around if @captives_around.any?
 		return move_to near_enemy_around if @enemies_around.any?
@@ -79,8 +82,8 @@ class Player
 			@warrior.bind! direction
 		end
 		
-		def attack_enemy
-			direction = @enemies_near.last
+		def attack_enemy(direction = nil)
+			direction = @enemies_near.last if direction == nil
 			@warrior.attack! direction
 		end
 		
@@ -90,7 +93,7 @@ class Player
 		end
 		
 		def next_empty
-			next_empty = @empties_near.shift
+			next_empty = @empties_near.pop
 			puts "Go to next empty #{next_empty}"
 			return next_empty
 		end
@@ -111,5 +114,23 @@ class Player
 		
 		def take_shelter
 			move_to next_empty
+		end
+		
+		def deactive_bomb
+			if not @ticking_near.empty?
+				direction = @ticking_near.first
+				puts "Deactive near bomb in direction #{direction}"
+				@warrior.rescue! direction
+			elsif not @ticking_around.empty?
+				direction = near_ticking_around
+				puts "Looking for the bomb around in direction #{direction}"
+				return move_to direction unless @warrior.feel(direction).enemy?
+				return attack_enemy direction
+			end
+		end
+		
+		def near_ticking_around
+			puts "Go to next ticking around"
+			@warrior.direction_of @ticking_around.last
 		end
 end
